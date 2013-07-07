73a5bd3f1a8e09e2c9885f7124c1e1a3ba6b18bd
moosader <racheljmorris@gmail.com>
2013-07-06 22:21:09 -0500

    Casting samples

---
+++ Lecture/10 Casting/Sample Code/Basic Casting/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int dollars, cents;
+
+	cout << "Enter amount of dollars: ";
+	cin >> dollars;
+
+	cout << "Enter amount of cents: ";
+	cin >> cents;
+
+	float total;
+
+	// No Cast
+	total = dollars + ( cents / 100 );
+	cout << "This won't work: $" << total << endl;
+
+	// C++ Style Cast
+	total = dollars + float( cents ) / 100;
+	cout << "This should work: $" << total << endl;
+
+	// C Style Cast
+	total = dollars + (float)cents / 100;
+	cout << "This should also work: $" << total << endl;
+
+	return 0;
+}
