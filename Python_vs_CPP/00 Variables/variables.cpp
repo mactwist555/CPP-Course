cfa92ce5f377d1149df1fd2498919a73b8dbc0c8
Rachel J Morris <RachelJMorris@gmail.com>
2015-10-25 16:41:04 -0500

    Variable comparison

---
+++ Python_vs_CPP/00 Variables/variables.cpp
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+int main()
+{
+	int myInteger = 30;
+	float myFloat = 29.99;
+	bool myBool = false;
+	char myChar = 'a';
+	string myString = "Hello World!" // to use this, #include <string> is needed
+	
+	return 0;
+}
---
+++ Python_vs_CPP/00 Variables/variables.py
@@ -0,0 +1,7 @@
+myInteger = 30
+myFloat = 29.99
+myBool = False
+# I don't think Python differentiates between chars and strings
+# You can use strings in Python with either single quote or double quote
+myChar = 'a'
+myString = "Hello World!"
