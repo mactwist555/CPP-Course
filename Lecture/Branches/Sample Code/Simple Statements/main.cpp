72ddf4f59a1597a1760270e9be020e2f8017f698
Rachel J Morris <racheljmorris@gmail.com>
2016-01-18 12:08:00 -0600

    Branching sample code

---
+++ Lecture/Branches/Sample Code/Simple Statements/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if ( number < 0 )
+    {
+        cout << "Your number is negative." << endl;
+    }
+
+    return 0;
+}
