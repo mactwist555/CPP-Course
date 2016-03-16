d2705d0b31ea3d2a09fa8feebfc17ae12597b50d
Rachel J. Morris <racheljmorris@gmail.com>
2016-03-16 00:23:39 -0500

    Update Menu.hpp

--- Sample_Code/Other/Rachels-Reusable-Menu/Menu.hpp
+++ Sample_Code/Other/Rachels-Reusable-Menu/Menu.hpp
@@ -9,30 +9,123 @@ class object.
 #include <iostream>
 using namespace std;
 
+class MenuOption
+{
+    public:
+    MenuOption() { }
+
+    MenuOption( const string& k, const string& v )
+    {
+        Setup( k, v );
+    }
+
+    void Setup( const string& k, const string& v )
+    {
+        key = k;
+        value = v;
+    }
+
+    string key;
+    string value;
+};
+
 class Menu
 {
     public:
-    static int ShowMenu( const string options[], int size );
+    static void Header( const string& header );
+    static void ShowMenu( const string options[], int size );
+    static void ShowMenu( const MenuOption options[], int size );
+    static void ShowMenu( const vector<MenuOption> options );
+    static int ShowMenuWithPrompt( const string options[], int size );
+    static int ShowMenuWithPrompt( const MenuOption options[], int size );
+    static int ShowMenuWithPrompt( const vector<MenuOption> );
     static int GetValidChoice( int min, int max );
+    static int GetValidChoice( const string& message, int min, int max );
     static void ClearScreen();
+    static void DrawHorizontalBar( int width, char symbol = '-' );
 
     private:
 };
 
-int Menu::ShowMenu( const string options[], int size )
+void Menu::DrawHorizontalBar( int width, char symbol )
+{
+    for ( int i = 0; i < width; i++ )
+    {
+        cout << symbol;
+    }
+    cout << endl;
+}
+
+void Menu::Header( const string& header )
+{
+    DrawHorizontalBar( 80 );
+    string head = "| " + header + " |";
+    cout << " " << head << endl << " ";
+    DrawHorizontalBar( head.size() );
+    cout << endl;
+}
+
+void Menu::ShowMenu( const string options[], int size )
 {
     for ( int i = 0; i < size; i++ )
     {
-        cout << (i+1) << ".\t" << options[i] << endl;
+        cout << " " << (i+1) << ".\t" << options[i] << endl;
     }
+}
+
+void Menu::ShowMenu( const MenuOption options[], int size )
+{
+    for ( int i = 0; i < size; i++ )
+    {
+        cout << " " << (i+1) << ".\t" << options[i].value << endl;
+    }
+}
+
+void Menu::ShowMenu( const vector<MenuOption> options )
+{
+    for ( int i = 0; i < options.size(); i++ )
+    {
+        cout << " " << (i+1) << ".\t" << options[i].value << endl;
+    }
+}
+
+int Menu::ShowMenuWithPrompt( const string options[], int size )
+{
+    ShowMenu( options, size );
     int choice = GetValidChoice( 1, size );
+    return choice - 1;
+}
+
+int Menu::ShowMenuWithPrompt( const MenuOption options[], int size )
+{
+    ShowMenu( options, size );
+    int choice = GetValidChoice( 1, size );
+    return choice - 1;
+}
+
+int Menu::ShowMenuWithPrompt( const vector<MenuOption> options )
+{
+    ShowMenu( options );
+    int choice = GetValidChoice( 1, options.size() );
     return choice - 1;
 }
 
 int Menu::GetValidChoice( int min, int max )
 {
+    return GetValidChoice( "", min, max );
+}
+
+int Menu::GetValidChoice( const string& message, int min, int max )
+{
+    if ( message != "" )
+    {
+        cout << endl;
+        DrawHorizontalBar( message.size() + 2 );
+        cout << " " << message << endl;
+    }
+
     int choice;
-    cout << ">> ";
+    cout << " >> ";
     cin >> choice;
 
     while ( choice < min || choice > max )
